45225cb83312a35c69880192c590171e18af6b54
vardanm1993 <vardanm1993@gmail.com>
2024-01-12 18:30:06 +0000

    Modified: add single book page logic

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -54,15 +54,20 @@ class BookController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Book $book)
     {
-        //
+        return view('books.show',
+            [
+                'book' => $book->load([
+                    'reviews' => fn($query) => $query->latest()
+                ])
+            ]);
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Book $book)
     {
         //
     }
@@ -70,7 +75,7 @@ class BookController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Book $book)
     {
         //
     }
@@ -78,7 +83,7 @@ class BookController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Book $book)
     {
         //
     }
