b256afcf9c70a5818a27e954e8acdf09ebd18074
vardanm1993 <vardanm1993@gmail.com>
2024-01-12 10:12:09 +0000

    Add app layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Book Reviews</title>
+    @vite('resources/css/app.css')
+</head>
+
+<body class="container mx-auto mt-10 mb-10 max-w-3xl">
+@yield('content')
+</body>
+
+</html>
