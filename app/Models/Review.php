17e12c26ea8a55da89cd49a08b47303d7693c795
vardanm1993 <vardanm1993@gmail.com>
2024-01-13 12:44:22 +0000

    Modified: add booted method for cache

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -17,5 +17,11 @@ class Review extends Model
         return $this->belongsTo(Book::class);
     }
 
+    public static function booted()
+    {
+        static::updated(fn(Review $review) =>cache()->forget('book:' . $review->book_id));
+        static::deleted(fn(Review $review) =>cache()->forget('book:' . $review->book_id));
+    }
+
 
 }
